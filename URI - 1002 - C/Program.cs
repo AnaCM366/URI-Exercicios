d594b45beb2b79201ddc875906752d7ae883887f
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-03 20:11:54 -0300

    EXERCICIO URI 1002

---
+++ URI - 1002 - C/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Globalization;
+
+decimal pi = 3.14159M;
+decimal raio = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+decimal area = pi * (raio * raio);
+Console.WriteLine("A=" + area.ToString("F4",CultureInfo.InvariantCulture));
