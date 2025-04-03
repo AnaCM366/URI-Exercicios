cab4fbd85e41f9ddbd35e1d0f380234f7cba645c
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-03 20:16:32 -0300

    NOVA PASTA

--- URI - 1002 - C/Program.cs
+++ URI - 1002 - C/Program.cs
@@ -1,6 +1,1 @@
-﻿using System.Globalization;
-
-decimal pi = 3.14159M;
-decimal raio = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-decimal area = pi * (raio * raio);
-Console.WriteLine("A=" + area.ToString("F4",CultureInfo.InvariantCulture));
+﻿
