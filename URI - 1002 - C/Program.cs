bd9945f0d4e94e07c139e58c66d4e123716aedcb
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-03 20:24:22 -0300

    EXERCICIO 1002 REALIZADO EM C# - Calculado a area de um raio.

--- URI - 1002 - C/Program.cs
+++ URI - 1002 - C/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using System.Globalization;
+
+decimal pi = 3.14159M;
+decimal raio = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+decimal area = pi * (raio * raio);
+Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
