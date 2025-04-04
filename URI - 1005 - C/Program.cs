b00f0ac83a4a43ef88f0f1556e53b3c5ffe761a7
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-03 21:55:08 -0300

    EXERCICIO 1005 REALIZADO EM C# - Calcule a media ponderada (prova com peso)

---
+++ URI - 1005 - C/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+double A = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+double B = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+double MEDIA = ((A * 3.5) + (B * 7.5)) / 11.0;
+
+Console.WriteLine("MEDIA = " + MEDIA.ToString("F5",CultureInfo.InvariantCulture));
